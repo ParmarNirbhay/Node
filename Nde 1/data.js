93dc583ab6ad6c4aa53335682b1ef53db1e333df
NIRBHAY PARMAR <parmarnirbhay715@gmail.com>
2023-10-13 23:12:51 +0530

    first commit

---
+++ Nde 1/data.js
@@ -0,0 +1,9 @@
+const data=[
+    {name:'Nirbhay Parmar', email:'parmarnirbhay715@gmail.com'},
+    {name:'sam', email:'paay715@gmail.com'},
+    {name:'piter', email:'parma15@gmail.com'}
+
+
+]
+
+    module.exports=data;
